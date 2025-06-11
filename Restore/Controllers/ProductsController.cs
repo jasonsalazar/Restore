30920b56d0bd10f357284bae3eba1f19e464fd1c
Jason Salazar <razalasnosaj@gmail.com>
2025-06-12 07:25:56 +0800

    Adding a Products API Controller, removed appsettings.json

---
+++ Restore/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restore.Data;
+using Restore.Entities;
+
+namespace Restore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController(StoreContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await context.Products.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await context.Products.FindAsync(id);
+
+            if (product == null) return NotFound();
+
+            return product;
+        }
+    }
+}

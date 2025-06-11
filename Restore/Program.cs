f5a3c328356981dcd3737e054e4cd3eb8c8e3eb4
Jason Salazar <razalasnosaj@gmail.com>
2025-06-12 05:34:02 +0800

    Add a DbContext class

--- Restore/Program.cs
+++ Restore/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Restore.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StoreContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 

e9cfc0cf4da1ec24d867fdab609b4de07932230e
Jason Salazar <razalasnosaj@gmail.com>
2025-06-12 06:13:51 +0800

    Initializing the database in code

--- Restore/Data/DbInitializer.cs
+++ Restore/Data/DbInitializer.cs
@@ -5,7 +5,7 @@ namespace Restore.Data
 {
     public class DbInitializer
     {
-        public void InitDb(WebApplication app)
+        public static void InitDb(WebApplication app)
         {
             using var scope = app.Services.CreateScope();
 
@@ -15,7 +15,7 @@ namespace Restore.Data
             SeedData(context);
         }
 
-        private void SeedData(StoreContext context)
+        private static void SeedData(StoreContext context)
         {
             context.Database.Migrate();
 

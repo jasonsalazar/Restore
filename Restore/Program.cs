34f910c46d29e421f1cf42e85ae15763d75d3d9e
Jason Salazar <razalasnosaj@gmail.com>
2025-06-13 02:25:55 +0800

    Adding CORS support to the API and HTTPS

--- Restore/Program.cs
+++ Restore/Program.cs
@@ -7,10 +7,18 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader()
+       .AllowAnyMethod()
+       .WithOrigins("https://localhost:3000");
+});
+
 app.MapControllers();
 
 DbInitializer.InitDb(app);

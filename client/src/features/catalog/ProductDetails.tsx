759210e308c162512e6d96b89aba2d9dd189a4fd
Jason Salazar <razalasnosaj@gmail.com>
2025-06-14 02:01:27 +0800

    Fetching an individual product from the API and styling the product details page

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,3 +1,92 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import type { Product } from "../../app/models/product";
+import {
+  Button,
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  TextField,
+  Typography,
+} from "@mui/material";
+
 export default function ProductDetails() {
-  return <div>ProductDetails</div>;
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    fetch(`https://localhost:7078/api/products/${id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+      .catch((error) => console.error("Error fetching product:", error));
+  }, [id]);
+
+  if (!product) return <div>Loading...</div>;
+
+  const ProductDetails = [
+    { label: "Name", value: product.name },
+    { label: "Description", value: product.description },
+    { label: "Type", value: product.type },
+    { label: "Brand", value: product.brand },
+    { label: "Quantity in stock", value: product.quantityInStock },
+  ];
+
+  return (
+    <Grid container spacing={6} maxWidth="lg" sx={{ marginX: "auto" }}>
+      <Grid size={6}>
+        <img
+          src={product.pictureUrl}
+          alt={product.name}
+          style={{ width: "100%" }}
+        />
+      </Grid>
+      <Grid size={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ marginBottom: 2 }} />
+        <Typography variant="h4" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <TableContainer>
+          <Table sx={{ "& td": { fontSize: "1rem" } }}>
+            <TableBody>
+              {ProductDetails.map((detail, index) => (
+                <TableRow key={index}>
+                  <TableCell sx={{ fontWeight: "bold" }}>
+                    {detail.label}
+                  </TableCell>
+                  <TableCell>{detail.value}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <Grid container spacing={2} marginTop={3}>
+          <Grid size={6}>
+            <TextField
+              variant="outlined"
+              type="number"
+              label="Quantity in basket"
+              fullWidth
+              defaultValue={1}
+            />
+          </Grid>
+          <Grid size={6}>
+            <Button
+              sx={{ height: "55px" }}
+              variant="contained"
+              color="primary"
+              size="large"
+              fullWidth
+            >
+              Add to basket
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }

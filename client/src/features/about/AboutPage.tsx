68be937223a9e28cef79ee32b74dbd11ea45e01e
Jason Salazar <razalasnosaj@gmail.com>
2025-06-18 03:51:12 +0800

    Adding an Error RTK Query API to the client, update the AboutPage to test the API response errors, adding toasts, using type guards, validation errors

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,3 +1,95 @@
+import {
+  Alert,
+  AlertTitle,
+  Button,
+  ButtonGroup,
+  Container,
+  List,
+  ListItem,
+  Typography,
+} from "@mui/material";
+import {
+  useLazyGet400ErrorQuery,
+  useLazyGet401ErrorQuery,
+  useLazyGet404ErrorQuery,
+  useLazyGet500ErrorQuery,
+  useLazyGetValidationErrorQuery,
+} from "./errorApi";
+import { useState } from "react";
+
 export default function AboutPage() {
-  return <div>AboutPage</div>;
+  const [validationErrors, setValidationErrors] = useState<string[]>([]);
+
+  const [get400Error] = useLazyGet400ErrorQuery();
+  const [get401Error] = useLazyGet401ErrorQuery();
+  const [get404Error] = useLazyGet404ErrorQuery();
+  const [get500Error] = useLazyGet500ErrorQuery();
+  const [getValidationError] = useLazyGetValidationErrorQuery();
+
+  const handleValidationError = async () => {
+    try {
+      await getValidationError().unwrap();
+    } catch (error: unknown) {
+      if (
+        error &&
+        typeof error === "object" &&
+        "message" in error &&
+        typeof (error as { message: unknown }).message === "string"
+      ) {
+        const errorArray = (error as { message: string }).message.split(", ");
+        setValidationErrors(errorArray);
+      }
+    }
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <Typography gutterBottom variant="h3">
+        Errors for testing
+      </Typography>
+      <ButtonGroup fullWidth>
+        <Button
+          variant="contained"
+          onClick={() => get400Error().catch((err) => console.log(err))}
+        >
+          Test 400 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() => get401Error().catch((err) => console.log(err))}
+        >
+          Test 401 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() => get404Error().catch((err) => console.log(err))}
+        >
+          Test 404 Error
+        </Button>
+
+        <Button
+          variant="contained"
+          onClick={() => get500Error().catch((err) => console.log(err))}
+        >
+          Test 500 Error
+        </Button>
+
+        <Button variant="contained" onClick={handleValidationError}>
+          Test Validation Error
+        </Button>
+      </ButtonGroup>
+      {validationErrors.length > 0 && (
+        <Alert severity="error">
+          <AlertTitle>Validation Errors</AlertTitle>
+          <List>
+            {validationErrors.map((err) => (
+              <ListItem key={err}>{err}</ListItem>
+            ))}
+          </List>
+        </Alert>
+      )}
+    </Container>
+  );
 }

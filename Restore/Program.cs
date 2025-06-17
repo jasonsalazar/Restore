9f86b521202f1d44ed569cb06b2f70c5be88cc0e
Jason Salazar <razalasnosaj@gmail.com>
2025-06-18 00:59:58 +0800

    Adding exception handling middleware

--- Restore/Program.cs
+++ Restore/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Restore.Data;
+using Restore.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddCors();
+builder.Services.AddTransient<ExceptionMiddleware>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(opt =>
 {
     opt.AllowAnyHeader()

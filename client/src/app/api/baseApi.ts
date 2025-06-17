20f5055605257cd8fb110918aa08f921d6c8de96
Jason Salazar <razalasnosaj@gmail.com>
2025-06-18 04:11:26 +0800

    Creating a server error component

--- client/src/app/api/baseApi.ts
+++ client/src/app/api/baseApi.ts
@@ -2,6 +2,7 @@ import type { BaseQueryApi, FetchArgs } from "@reduxjs/toolkit/query";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 import { startLoading, stopLoading } from "../layout/uiSlice";
 import { toast } from "react-toastify";
+import { router } from "../routes/Routes";
 
 const customBaseQuery = fetchBaseQuery({
   baseUrl: "https://localhost:7078/api",
@@ -47,8 +48,8 @@ export const baseQueryWithErrorHandling = async (
         break;
 
       case 500:
-        if (typeof responseData === "object" && "title" in responseData)
-          toast.error(responseData.title);
+        if (typeof responseData === "object")
+          router.navigate("/server-error", { state: { error: responseData } });
         break;
     }
   }

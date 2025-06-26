efdbe33df4222bb33c7501bfdf108731093bd3fa
Jason Salazar <razalasnosaj@gmail.com>
2025-06-27 00:32:19 +0800

    Updating the basket item count in the nav bar

--- client/src/app/models/basket.ts
+++ client/src/app/models/basket.ts
@@ -1,9 +1,21 @@
+import type { Product } from "./product";
+
 export interface Basket {
   basketId: string;
   items: Item[];
 }
 
-export interface Item {
+export class Item {
+  constructor(product: Product, quantity: number) {
+    this.productId = product.id;
+    this.name = product.name;
+    this.price = product.price;
+    this.pictureUrl = product.pictureUrl;
+    this.brand = product.brand;
+    this.type = product.type;
+    this.quantity = quantity;
+  }
+
   productId: number;
   name: string;
   price: number;

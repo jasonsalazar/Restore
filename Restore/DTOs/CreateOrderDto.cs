95a68721073446a12772934a302ed957f3d1a3cc
Jason Salazar <razalasnosaj@gmail.com>
2025-09-12 01:02:56 +0800

    Adding the Create Order logic in the controller

---
+++ Restore/DTOs/CreateOrderDto.cs
@@ -0,0 +1,10 @@
+﻿using Restore.Entities.OrderAggregate;
+
+namespace Restore.DTOs
+{
+    public class CreateOrderDto
+    {
+        public required ShippingAddress ShippingAddress { get; set; }
+        public required PaymentSummary PaymentSummary { get; set; }
+    }
+}

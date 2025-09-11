6f4bc851a17888e7afc33560eb760d52b23a8035
Jason Salazar <razalasnosaj@gmail.com>
2025-09-12 00:19:02 +0800

    Creating the orders controller

---
+++ Restore/Controllers/OrdersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restore.Data;
+using Restore.Entities.OrderAggregate;
+using Restore.Extensions;
+
+namespace Restore.Controllers
+{
+    [Authorize]
+    public class OrdersController(StoreContext context) : BaseApiController
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Order>>> GetOrders()
+        {
+            var orders = await context.Orders
+                .Include(x => x.OrderItems)
+                .Where(x => x.BuyerEmail == User.GetUsername())
+                .ToListAsync();
+
+            return orders;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Order>> GetOrderDetails(int id)
+        {
+            var order = await context.Orders
+                .Where(x => x.BuyerEmail == User.GetUsername() && x.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (order == null) return NotFound();
+
+            return order;
+        }
+    }
+}
---
+++ Restore/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace Restore.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            return user.Identity?.Name ?? throw new UnauthorizedAccessException();
+        }
+    }
+}
